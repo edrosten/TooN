bcb8ea12e7fdad98d4cd310814524dc44e4b4ac6
edrosten <edrosten>
2009-03-20 12:23:58 +0000

    try varargs.

--- test/makevector.cc
+++ test/makevector.cc
@@ -1,5 +1,22 @@
 #include <TooN/TooN.h>
+#include <cstdarg>
 using namespace TooN;
+
+
+template<int N, typename Precision> Vector<N, Precision> makeVector(const Precision& x, ...)
+{
+	Vector<N> retval;
+	retval[0] = N;
+
+	va_list ap;
+	int i;
+	va_start(ap, x);
+	for(int i=1; i < N; i++)
+		retval[i] = va_arg(ap, Precision);
+	va_end(ap);
+	return retval;
+
+}
 
 extern "C"{
 double use_make_vector_double(const Vector<4>& v)
@@ -10,6 +27,11 @@ double use_make_vector_double(const Vector<4>& v)
 double use_make_vector_int(const Vector<4>& v)
 {
 	return v * makeVector<int>(0,0,2,0);
+}
+
+double use_make_vector_var(const Vector<4>& v)
+{
+	return v * makeVector<4, double>(0.0,0.0,2.0,0.0);
 }
 
 }
