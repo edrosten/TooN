7f59831e9b5d3c2e38c272e39896ae986e065a08
twd20 <twd20>
2009-03-30 19:17:21 +0000

    fixed this test so it compiles with Vector(Zero) rather than Zero(Vector)

--- test/test3.cc
+++ test/test3.cc
@@ -11,11 +11,8 @@ template<class C> void type(const C&)
 
 int main()
 {
-	Matrix<3> m1;
-	Matrix<3> m2;
-
-	Zero(m1);
-	Zero(m2);
+	Matrix<3> m1(Zero);
+	Matrix<3> m2(Zero);
 	
 	m1.slice<0,0,2,2>()+=3;
 	m2.slice<1,1,2,2>()+=2;
