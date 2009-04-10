dd1b75a29f5ee7f77c005f10662a4dc696755678
twd20 <twd20>
2009-04-10 00:31:06 +0000

    correction to library name

--- Cholesky.h
+++ Cholesky.h
@@ -2,7 +2,7 @@
 
 //     Copyright (C) 2009 Tom Drummond (twd20@cam.ac.uk)
 //
-// This file is part of the GNU ISO C++ Library.  This library is free
+// This file is part of the TooN Library.  This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 2, or (at your option)
