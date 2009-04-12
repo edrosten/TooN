550364275903434f4eb51480756fd97c567d5b25
twd20 <twd20>
2009-04-12 09:48:55 +0000

    added GPLV2 + special exception to a bunch of files

--- internal/reference.hh
+++ internal/reference.hh
@@ -1,5 +1,33 @@
 // -*- c++ -*-
 
+// Copyright (C) 2009 Tom Drummond (twd20@cam.ac.uk),
+// Ed Rosten (er258@cam.ac.uk)
+//
+// This file is part of the TooN Library.  This library is free
+// software; you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2, or (at your option)
+// any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along
+// with this library; see the file COPYING.  If not, write to the Free
+// Software Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+// USA.
+
+// As a special exception, you may use this file as part of a free software
+// library without restriction.  Specifically, if other files instantiate
+// templates or use macros or inline functions from this file, or you compile
+// this file and link it with other files to produce an executable, this
+// file does not by itself cause the resulting executable to be covered by
+// the GNU General Public License.  This exception does not however
+// invalidate any other reasons why the executable file might be covered by
+// the GNU General Public License.
+
 ////////////////////////////////////////////////////////////////////////////////
 //
 // Helper classes for matrices constructed as references to foreign data
@@ -25,7 +53,11 @@ struct Reference
 		struct MLayout
 			: public Internal::GenericMBase<Rows, Cols, Precision, (Cols==-1?-2:Cols), 1, Internal::MatrixSlice<Rows, Cols, Precision> >
 		{
-			MLayout(Precision* p, int r=0, int c=0)
+
+			MLayout(Precision* p)
+				: Internal::GenericMBase<Rows,Cols,Precision, (Cols==-1?-2:Cols), 1, Internal::MatrixSlice<Rows, Cols, Precision> > (p, 0, 0, 0, 0)
+			{}
+			MLayout(Precision* p, int r, int c)
 				: Internal::GenericMBase<Rows,Cols,Precision, (Cols==-1?-2:Cols), 1, Internal::MatrixSlice<Rows, Cols, Precision> > (p, r, c, 0, 0)
 			{}
 		};
@@ -35,7 +67,10 @@ struct Reference
 	{
 		template<int Rows, int Cols, class Precision> struct MLayout: public Internal::GenericMBase<Rows, Cols, Precision, 1, (Rows==-1?-2:Rows), Internal::MatrixSlice<Rows, Cols, Precision> >
 		{
-			MLayout(Precision* p, int r=0, int c=0)
+			MLayout(Precision* p)
+				: Internal::GenericMBase<Rows, Cols, Precision, 1, (Rows==-1?-2:Rows), Internal::MatrixSlice<Rows, Cols, Precision> >(p, 0, 0, 0, 0)
+			{}
+			MLayout(Precision* p, int r, int c)
 				: Internal::GenericMBase<Rows, Cols, Precision, 1, (Rows==-1?-2:Rows), Internal::MatrixSlice<Rows, Cols, Precision> >(p, r, c, 0, 0)
 			{}
 		};
