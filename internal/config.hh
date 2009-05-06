796d86a9c895848803fcf2fb403db02b49e63864
edrosten <edrosten>
2009-05-06 14:58:54 +0000

    Blank file again.

--- internal/config.hh
+++ internal/config.hh
@@ -1,13 +1,1 @@
-/* internal/config.hh.  Generated from config.hh.in by configure.  */
-/* #undef TOON_TYPEOF_DECLTYPE */
-
-/* #undef TOON_TYPEOF_TYPEOF */
-
-#define TOON_TYPEOF___TYPEOF__ 1
-
-/* #undef TOON_TYPEOF_BOOST */
-
-/* #undef TOON_TYPEOF_BUILTIN */
-
-#define PACKAGE_VERSION "version-2.0.0-beta2"
 
