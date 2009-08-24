fc27fca81cfae8d706a7e68006da6706f0f90123
edrosten <edrosten>
2009-08-24 13:06:58 +0000

    Another test program.

---
+++ test/slice_test.cc
@@ -0,0 +1,24 @@
+#include <TooN/TooN.h>
+using namespace std;
+using namespace TooN;
+
+int main()
+{
+	Vector<6> v = makeVector(1, 2, 3, 4, 5, 6);
+	Vector<>  u = v;
+
+	cout << v.slice<0,3>() << endl;
+	cout << u.slice<0,3>() << endl << endl;
+
+	cout << v.slice<1,3>(1, 3) << endl;
+	cout << u.slice<1,3>(1, 3) << endl;
+	cout << v.slice<Dynamic,3>(1, 3) << endl;
+	cout << u.slice<Dynamic,3>(1, 3) << endl;
+	cout << v.slice<1,Dynamic>(1, 3) << endl;
+	cout << u.slice<1,Dynamic>(1, 3) << endl;
+	cout << v.slice<Dynamic,Dynamic>(1, 3) << endl;
+	cout << u.slice<Dynamic,Dynamic>(1, 3) << endl << endl;
+
+	cout << v.slice(2, 3) << endl;
+	cout << u.slice(2, 3) << endl << endl;
+}
