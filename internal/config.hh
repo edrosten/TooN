3ca20a837cc9dd1aafc6779854605a92f71600e7
edrosten <edrosten>
2010-01-11 14:54:47 +0000

    Blank config.hh

--- internal/config.hh
+++ internal/config.hh
@@ -1,14 +1,1 @@
-/* internal/config.hh.  Generated from config.hh.in by configure.  */
-/* #undef TOON_TYPEOF_DECLTYPE */
 
-/* #undef TOON_TYPEOF_TYPEOF */
-
-/* #undef TOON_TYPEOF___TYPEOF__ */
-
-/* #undef TOON_TYPEOF_BOOST */
-
-/* #undef TOON_TYPEOF_BUILTIN */
-
-/* #undef TOON_DEPRECATED_GCC */
-
-#define TOON_USE_LAPACK 1
