5ac85ad5eb77789f5f8a5f306e82da7b7e8655ed
edrosten <edrosten>
2010-02-10 15:24:18 +0000

    Fixed/updated regression test for complex.

--- regressions/complex.cc
+++ regressions/complex.cc
@@ -1,5 +1,5 @@
 #include <complex>
-#include "regressions/regression.h"
+#include "TooN/regressions/regression.h"
 #include <TooN/internal/planar_complex.hh>
 
 int main()
@@ -33,5 +33,22 @@ int main()
 	imag[3] = 10;
 		
 	cout << vec << endl;
+	
+	
+	// Matrix tests
+	Matrix<4,4,complex<double> > A;
+	A[0] = makeVector<complex<double> >(1.+i, 1.+6.*i, 3, 1.-i);
+	A[1] = makeVector<complex<double> >(3.+6.i, 9.+2.*i, 4, 6.-1.*i);
+	A[2] = makeVector<complex<double> >(1.+8.i, 2.*i, 2.-1.*i, 4.-1.*i);
+	A[3] = makeVector<complex<double> >(2.+1.*i, 1.+3.*i, 1, 12.-5.*i);
+	
+	cout << A;
+	cout << A * vec<<endl;
+	cout << A.T() ;
+	cout << A * A ;
 
+	Vector<Dynamic, complex<double> > v3 = makeVector(1, 2, 3);
+	cout << v3 << endl;
+	cout << v3.size() << endl;
+	
 }
