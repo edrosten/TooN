facecdf662be00bdbf7cc9ac2e41a7814ccd8482
Edward Rosten <erosten@snap.com>
2017-05-31 14:07:56 +0100

    Update regressions to support commit message

--- regressions/initializer_list.cc
+++ regressions/initializer_list.cc
@@ -8,9 +8,21 @@ int main()
 	Vector<3> v({1, 2, 3});
 	cout << v << endl;
 
+	Vector<5> v6 = {{1,2,3,4,5}};
+	cout << v6 << endl;
+	
+	//Not supported
+	//Vector<5> v = {1,2,3,4,5};
+
 	Vector<Dynamic> v2 = {1, 2, 3, 4};
 	cout << v2 << endl;
 	
 	Vector<Resizable> v3 = {1, 2, 3, 4, 5};
 	cout << v3 << endl;
+
+	Vector<Resizable> v4 = {{1, 2, 3, 4, 5, 6}};
+	cout << v4 << endl;
+
+	Vector<Resizable> v5({1,2});
+	cout << v5 << endl;
 }
